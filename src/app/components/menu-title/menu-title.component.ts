2347c6d10144548b65f71eb836f8d2055ea6cc71
ygofarias <ygo.f@hotmail.com>
2023-12-23 07:26:15 -0300

    content created

--- src/app/components/menu-title/menu-title.component.ts
+++ src/app/components/menu-title/menu-title.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-title',
   templateUrl: './menu-title.component.html',
   styleUrls: ['./menu-title.component.css','./menu-title.responsive.component.css']
 })
-export class MenuTitleComponent {
+export class MenuTitleComponent implements OnInit{
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,17 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "novo headset",
+    "description": "axl999",
+    "photoCover": "https://m.media-amazon.com/images/I/61Sh5LRZJoL._AC_SL1000_.jpg"
+
+  },
+
+  {
+    "id": "2",
+    "title": "novo headset",
+    "description": "Top!",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtTnvCfmVbooAZ7arQDvrxu3RpHAQ-N-MryHUQQUZ0WCVDB6Qb5hZSkK17jltj9VAUhFg&usqp=CAU"
+
+  }
+]
